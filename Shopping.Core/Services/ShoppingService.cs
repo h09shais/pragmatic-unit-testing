0b1f181cb524cc44771a30ba6fec435b6bbe0917
Shanawaz Islam <sis@cfhcloud.com>
2021-02-03 16:08:34 +0100

    More Func in action

--- Shopping.Core/Services/ShoppingService.cs
+++ Shopping.Core/Services/ShoppingService.cs
@@ -7,36 +7,25 @@ namespace Shopping.Core.Services
     public class ShoppingService
     {
         public static void Checkout(
-            IEnumerable<int> itemIds, 
-            int memberId,
             string promoCode,
             DateTime when,
-            Func<int, Member> findMemberById,
-            Func<IEnumerable<int>, IEnumerable<Item>> findItemsByIds,
-            Action<LogLevel, string> log,
-            Action<int, decimal> chargeMember)
+            Func<DateTime> findMember,
+            Func<IEnumerable<Item>> findItems,
+            Action<decimal> chargeMember)
         {
-            var member = findMemberById(memberId);
-            if (member == null)
-            {
-                throw new MemberNotFoundException(memberId);
-            }
-
-            var items = findItemsByIds(itemIds);
-
             // decide birthday discount
-            var birthdayDiscountPercentage = Calculate.DiscountForMemberBirthday(when, member.Birthday);
+            var birthdayDiscountPercentage = Calculate.DiscountForMemberBirthday(when, findMember());
 
             // decide promo discount
             var promoDiscountPercentage = Calculate.DiscountForPromoCode(promoCode, when);
 
             // decide applicable discount and create purchases
-            var totalPayable = Calculate.TotalPayable(birthdayDiscountPercentage, promoDiscountPercentage, items);
+            var totalPayable = Calculate.TotalPayable(birthdayDiscountPercentage, promoDiscountPercentage, findItems());
 
             // log and persist
-            log(LogLevel.Info, $"We got member {member.Name} hooked!");
+            //log(LogLevel.Info, $"We got member {member.Name} hooked!");
 
-            chargeMember(memberId, totalPayable);
+            chargeMember(totalPayable);
         }
     }
 }
